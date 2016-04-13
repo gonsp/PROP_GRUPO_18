415bfaa1a91cdcde0dcb028c528f5b4e219eff38
Victor <victor.massague@est.fib.upc.edu>
2016-04-13 11:11:47 +0200

    persistence

---
+++ src/common/persistence/NodeSerializer.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import java.io.Serializable;
+
+
+public abstract class NodeSerializer implements Serializable {
+
+    private Node node = null;
+    private String data = null;
+
+    public NodeSerializer(Node node) {
+        this.node = node;
+    }
+
+    public NodeSerializer(String data) {
+        this.data = data;
+    }
+
+    public Node inflate() {
+        if (node == null) {
+
+        }
+        return node;
+    }
+
+    public String deflate();
+}
