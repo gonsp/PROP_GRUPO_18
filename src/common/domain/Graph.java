f61c47fe590729848dae68625d3f518e9efeaed5
Guillermo <guillermomm1230@gmail.com>
2016-04-14 22:08:31 +0200

    Graph compila

--- src/common/domain/Graph.java
+++ src/common/domain/Graph.java
@@ -3,95 +3,95 @@ package domain;
 import java.util.HashMap;
 
 public class Graph {
-        //Atributos
+    //Attributes
 	private String name;
-
-        private NodeContainer authors;
-        private NodeContainer papers;
-        private NodeContainer terms;
-        private NodeContainer labels;
-        private NodeContainer conferences;
+	private NodeContainer authors;
+	private NodeContainer papers;
+	private NodeContainer terms;
+	private NodeContainer labels;
+	private NodeContainer conferences;
 	
 	//Constructors
-        public Graph() {
-                authors = new NodeContainer();
-                papers = new NodeContainer();
-                terms = new NodeContainer();
-                labels = new NodeContainer();
-                conferences = new NodeContainer();
+	public Graph() {
+		authors = new NodeContainer();
+		papers = new NodeContainer();
+		terms = new NodeContainer();
+		labels = new NodeContainer();
+		conferences = new NodeContainer();
 	}
 	
-        public Graph(String name) {
+    public Graph(String name) {
 		this.name = name;
-		Graph();
+		authors = new NodeContainer();
+		papers = new NodeContainer();
+		terms = new NodeContainer();
+		labels = new NodeContainer();
+		conferences = new NodeContainer();
 	}
 	
 	//Get & Set
-        public String getName() {
+	public String getName() {
 		return name;
 	}
 	
-        public void setName(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 	
-        public void setLastId(int lastUsed, NodeType type) throws GraphException {
-                getNodeContainerFromNodeType(type).setLastID(ID);
+	public void setLastId(int lastUsed, NodeType type) throws GraphException {
+		getNodeContainer(type).setLastID(lastUsed);
 	}
 	
-        public int getSize(NodeType type) throws GraphException {
-		return getNodeContainerFromNodeType(type).getSize();
+	public int getSize(NodeType type) throws GraphException {
+		return getNodeContainer(type).getSize();
 	}
 	
 	//Graph Edition
-        public void addNode(Node node) {
-                getNodeContainerFromNodeType(getNodeTypeFromNode(node)).addNode(node);
+    public void addNode(Node node) throws GraphException {
+		getNodeContainer(getNodeType(node)).addNode(node);
 	}
 	
-        public void addNode(Node node, int id) throws GraphException {
-                getNodeContainerFromNodeType(getNodeTypeFromNode(node)).addNode(node, id);
+	public void addNode(Node node, int id) throws GraphException {
+		getNodeContainer(getNodeType(node)).addNode(node, id);
 	}
 	
-        public void removeNode(NodeType type, int id) throws GraphException {
-		getNodeContainerFromNodeType(type).removeNode(id);
+	public void removeNode(NodeType type, int id) throws GraphException {
+		getNodeContainer(type).removeNode(id);
 	}
 
 	//Queries
-        public Node getNode(NodeType type, int id) throws GraphException {
-		return getNodeContainerFromNodeType(type).getNode(id);
+	public Node getNode(NodeType type, int id) throws GraphException {
+		return getNodeContainer(type).getNode(id);
 	}
 	
-	private NodeType getNodeTypeFromNode(Node node) {
-                if(node instanceof Author) {
-                        return AUTHOR;
-                } else if(node instanceof Paper) {
-                        return PAPER;
-                } else if(node instanceof Term) {
-                        return TERM;
-                } else if(node instanceof Label) {
-                        return LABEL;
-                } else {
-                        return CONFERENCE
-                }
-        }
+	private NodeType getNodeType(Node node) {
+		if(node instanceof Author) {
+			return NodeType.AUTHOR;
+		} else if(node instanceof Paper) {
+			return NodeType.PAPER;
+		} else if(node instanceof Term) {
+			return NodeType.TERM;
+		} else if(node instanceof Label) {
+			return NodeType.LABEL;
+		} else {
+			return NodeType.CONFERENCE;
+		}
+	}
 	
-        private NodeContainer getNodeContainerFromNodeType(NodeType type) {
-                switch(type) {
+	private NodeContainer getNodeContainer(NodeType type) throws GraphException {
+        switch(type) {
 			case AUTHOR:
 				return authors;
-				break;
 			case PAPER:
-                                return papers;
-                                break;
+                return papers;
 			case TERM:
 				return terms;
-				break;
 			case LABEL:
 				return labels;
-				break;
 			case CONFERENCE:
 				return conferences;
-				break;
+			default: 
+				throw new GraphException(GraphExceptionError.TYPE_INVALID);
 		}
-        }
+	}
 }
