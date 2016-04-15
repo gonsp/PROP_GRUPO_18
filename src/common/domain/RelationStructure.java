ca35fdf59227d0d92ad3768b5f3be3d007816c8e
Adrià Farrés <14farresa@gmail.com>
2016-04-15 12:42:13 +0200

    Add RelationStructure

---
+++ src/common/domain/RelationStructure.java
@@ -0,0 +1,23 @@
+package common.domain;
+
+import java.util.ArrayList;
+
+public class RelationStructure
+{
+	private ArrayList<Relation> st;
+
+	// TODO
+	public RelationStructure()
+	{
+	}
+
+	// TODO
+	// Returns the relation at position i
+	public Relation getRelation(int i) throws IndexOutOfBoundsException
+	{
+		if (i >= st.size())
+			throw new IndexOutOfBoundsException("getRelation: index is out " +
+					"of range");
+		return st[i];
+	}
+}
