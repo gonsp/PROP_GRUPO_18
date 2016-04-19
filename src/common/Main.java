bdde186a09f8c3d00f36d003a7ed6b72878e8991
Pau Argelaguet <pau@outlook.com>
2016-04-19 15:13:28 +0200

    First reading textfiles test

--- src/common/Main.java
+++ src/common/Main.java
@@ -3,14 +3,14 @@ package common;
 import common.domain.Graph;
 import common.persistence.PersistenceController;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Graph g = new Graph();
         PersistenceController pc = new PersistenceController();
 
-        pc.importAuthor("test.txt");
+        pc.importAuthor("data/test.txt");
 
     }
 
--- src/common/persistence/PersistenceController.java
+++ src/common/persistence/PersistenceController.java
@@ -1,13 +1,15 @@
 package common.persistence;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import common.domain.Graph;
 
 public class PersistenceController {
 
-    public void importAuthor(String path) {
+    public void importNode(Graph g, String path, String type) {
 
         BufferedReader br = null;
 
@@ -15,7 +17,9 @@ public class PersistenceController {
 
             String sCurrentLine;
 
-            br = new BufferedReader(new FileReader(path));
+            String filePath = new File(path).getAbsolutePath();
+            FileReader fr = new FileReader(filePath);
+            br = new BufferedReader(fr);
 
             while ((sCurrentLine = br.readLine()) != null) {
                 System.out.println(sCurrentLine);
