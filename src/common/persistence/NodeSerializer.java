f9ba2dc66ccb5dae1851b4b5f527b70340a8a62a
Carlos Marchal <cmarchal@localhost.localdomain>
2016-04-19 15:56:07 +0200

    Node Serializer Update

--- src/common/persistence/NodeSerializer.java
+++ src/common/persistence/NodeSerializer.java
@@ -2,12 +2,33 @@ package common.persistence;
 
 import java.io.Serializable;
 
+import common.domain.Node;
+
 
 public abstract class NodeSerializer implements Serializable {
 
-    /*
     private Node node = null;
     private String data = null;
+
+    private int nodeId;
+    private String name;
+    
+    protected void inflate(String text) {
+        if (node == null) {
+            int m = text.indexOf('\t');
+            nodeId = Integer.parseInt(text.substring(0,m-1));
+            name = text.substring(m+1);
+        }
+    }    
+
+    protected void deflate() {
+        if (data == null) {
+            data = new String();
+            data.concat(node.nodeId.toString());
+            data.concat("\t");
+            data.concat(node.name);
+        }
+    }
 
     public NodeSerializer(Node node) {
         this.node = node;
@@ -16,15 +37,12 @@ public abstract class NodeSerializer implements Serializable {
     public NodeSerializer(String data) {
         this.data = data;
     }
-
-    public Node inflate() {
-        if (node == null) {
-
-        }
-        return node;
+    
+    public abstract Node getNode(); 
+    
+    public String getData() {
+        this.deflate();
+        return data;
     }
-
-    public String deflate();
-    */
 
 }
