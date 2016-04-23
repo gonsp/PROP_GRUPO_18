66fa631f9b622e6e22db5b35adb485f43092280d
Gonzalo Solera <gonzalo1796@gmail.com>
2016-04-23 19:48:48 +0200

    reduced matrix implemented

--- src/common/domain/Matrix.java
+++ src/common/domain/Matrix.java
@@ -1,5 +1,7 @@
 package common.domain;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 
 public class Matrix {
@@ -13,12 +15,45 @@ public class Matrix {
     }
 
     public double get(int i, int j) {
-        if(i >= 0 && i < n && j > 0 && j < m) {
-            return 0;
-            //int res = binarySearch();
+        if(checkPos(i, j)) {
+            Pair<Boolean, Integer> res = binarySearch(rows[i], j);
+            if(!res.getKey()) {
+                return 0;
+            } else {
+                return rows[i].get(res.getValue()).value;
+            }
         } else {
             return -1;
         }
+    }
+
+    public void put(int i, int j, double value) {
+        if(checkPos(i, j) && value != 0) {
+            ArrayList<Cell> row = rows[i];
+            Pair<Boolean, Integer> res = binarySearch(row, j);
+            if(!res.getKey()) {
+                row.add(res.getValue(), new Cell(j, value));
+            } else {
+                row.get(res.getValue()).value = value;
+            }
+        }
+    }
+
+    public Matrix mul(Matrix mat) {
+        if(this.m != mat.n) {
+            return null;
+        }
+        Matrix res = new Matrix(this.n, mat.m);
+        for(int i = 0; i < this.n; ++i) {
+            for(int j = 0; j < mat.m; ++j) {
+                double value = 0;
+                for(int k = 0; k < this.m; ++k) {
+                    value += this.get(i, k) * mat.get(k, j);
+                }
+                res.put(i, j, value);
+            }
+        }
+        return res;
     }
 
     public int getRows() {
@@ -28,11 +63,29 @@ public class Matrix {
     public int getColumns() {
         return m;
     }
-/*
-    private int binarySearch(ArrayList<Cell> row, int j) {
 
-    }*/
+    private Pair<Boolean, Integer> i_binarySearch(ArrayList<Cell> row, int l, int r, int j) {
+        if(l > r) {
+            return new Pair<Boolean, Integer>(false, l);
+        } else {
+            int m = (l+r)/2;
+            if(row.get(m).column < j) {
+                return i_binarySearch(row, l, m-1, j);
+            } else if(row.get(m).column > j) {
+                return i_binarySearch(row, m+1, r, j);
+            } else {
+                return new Pair<Boolean, Integer>(true, m);
+            }
+        }
+    }
 
+    private Pair<Boolean, Integer> binarySearch(ArrayList<Cell> row, int j) {
+        return i_binarySearch(row, 0, row.size()-1, j);
+    }
+
+    private boolean checkPos(int i, int j) {
+        return i >= 0 && i < n && j > 0 && j < m;
+    }
 
     private class Cell {
 
