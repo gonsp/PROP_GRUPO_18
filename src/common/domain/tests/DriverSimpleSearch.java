76b874807a13a26c031038b9b33fb9c024f0bec0
David Álvarez <davidalro@gmail.com>
2016-04-25 00:02:24 +0200

    Bugfixes

--- src/common/domain/tests/DriverSimpleSearch.java
+++ src/common/domain/tests/DriverSimpleSearch.java
@@ -4,17 +4,17 @@ import common.domain.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class DriverRelationalSearch {
+public class DriverSimpleSearch {
     private Graph g;
 
-    public void DriverRelationalSearch() {
-        StrubGraph sb = new StubGraph();
+    public void DriverSimpleSearch() {
+        StubGraph sb = new StubGraph();
         g = sb.getGraph();
     }
 
     private NodeType getNodeType(int nt) {
         if (nt == 1) return NodeType.AUTHOR;
-        else if (nt == 2) return NodeType.CONFERENCE;
+        else if (nt == 2) return NodeType.CONF;
         else if (nt == 3) return NodeType.TERM;
         else return NodeType.PAPER;
     }
@@ -26,17 +26,17 @@ public class DriverRelationalSearch {
         Scanner input = new Scanner(System.in);
         nodtype = input.nextInt();
         while (nodtype != 0) {
-            System.out.println("Enter patter to search: (or quit to go back)");
+            System.out.println("Enter pattern to search: (or quit to go back)");
             String pattern = input.next();
             while(!pattern.equals("quit")) {
                 SimpleSearch ss = new SimpleSearch(g, this.getNodeType(nodtype), pattern);
                 ss.search();
-                ArrayList<Result> res = ss.getResults();
+                ArrayList<GraphSearch.Result> res = ss.getResults();
                 for (int i = 0; i < res.size(); i++) {
                     System.out.println("Node: " + res.get(i).from.getValue());
                 }
                 System.out.println("Enter patter to search: (or quit to go back)");
-                String pattern = input.next();
+                pattern = input.next();
             }
             System.out.println("Enter node type: (1 - Author, 2 - Conference, 3 - Term, 4 - Paper,  0 - EXIT)");
             nodtype = input.nextInt();
