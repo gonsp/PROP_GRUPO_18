c70bbc05629d09528ae72edb6f689070f9b2bd0e
Pau Argelaguet <pau@outlook.com>
2016-05-29 13:29:12 +0200

    Bugfix importing weird files

--- src/common/persistence/FileHandler.java
+++ src/common/persistence/FileHandler.java
@@ -7,73 +7,67 @@ import java.util.List;
 
 public final class FileHandler {
 
-    protected static List<String> readFile(String path) {
+    public static List<String> readFile(String path) {
         List<String> toReturn = new ArrayList<>();
         try {
             String absolutePath = new File(path).getAbsolutePath();
             FileReader fr = new FileReader(absolutePath);
             BufferedReader br = new BufferedReader(fr);
             String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) {
+            while ((sCurrentLine = br.readLine()) != null)
                 toReturn.add(sCurrentLine);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return toReturn;
     }
 
-    protected static void writeFile(String path, List<String> strings) {
+    public static void writeFile(String path, List<String> strings, boolean append) {
         try {
             String absolutePath = new File(path).getAbsolutePath();
             File file = new File(absolutePath);
             file.createNewFile();
-            FileWriter fw = new FileWriter(file, true);
+            FileWriter fw = new FileWriter(file, append);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter out = new PrintWriter(bw);
-            for (String s : strings) {
+            for (String s : strings)
                 out.println(s);
-            }
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    protected static List<String> readDir(String path) {
+    public static List<String> readDir(String path) {
         List<String> toReturn = new ArrayList<>();
         String absolutePath = new File(path).getAbsolutePath();
         File folder = new File(absolutePath);
         if (folder.exists()) {
             File flist[] = folder.listFiles();
-            for (int i = 0; i < flist.length; i++) {
-                toReturn.add(flist[i].getName());
-            }
+            for (File aFlist : flist) toReturn.add(aFlist.getName());
         }
         return toReturn;
     }
 
-    protected static void clearDir(String path) {
+    public static void clearDir(String path) {
         String absolutePath = new File(path).getAbsolutePath();
         File folder = new File(absolutePath);
         if (!folder.exists()) {
             folder.mkdir();
         }
         File flist[] = folder.listFiles();
-        for (int i = 0; i < flist.length; i++) {
-            String pes = flist[i].getName();
-            if (pes.endsWith(".txt")) {
-                flist[i].delete();
-            }
+        for (File aFlist : flist) {
+            String pes = aFlist.getName();
+            if (pes.endsWith(".txt"))
+                aFlist.delete();
         }
     }
 
-    protected static String handlePath(String path){
+    public static String handlePath(String path){
         int last = path.length() - 1;
         if(last > -1){
-            if(!path.substring(last).equals("/")){
+            if(!path.substring(last).equals("/"))
                 return path + "/";
-            }
         } else {
             // TODO: Raise exception
         }
--- src/common/persistence/PersistenceController.java
+++ src/common/persistence/PersistenceController.java
@@ -1,9 +1,9 @@
 package common.persistence;
 
+import java.io.File;
 import java.util.*;
 
 import common.domain.*;
-
 import static common.domain.NodeType.*;
 import static common.persistence.FileHandler.*;
 
@@ -17,44 +17,41 @@ public class PersistenceController {
     public PersistenceController(Graph graph) {
         this.graph = graph;
         try {
-            // Adding implicit Label nodes
             graph.addNode(graph.createNode(LABEL, "Database"), 0);
             graph.addNode(graph.createNode(LABEL, "Data Mining"), 1);
             graph.addNode(graph.createNode(LABEL, "AI"), 2);
-            graph.addNode(graph.createNode(LABEL, "Information Retreival"), 3);
+            graph.addNode(graph.createNode(LABEL, "Information Retrieval"), 3);
         } catch (GraphException e) {
             e.printStackTrace();
         }
     }
 
-    public void exportNode(String path, NodeType n){
-        List<String> strings = new ArrayList<>();
-        Container<Node>.ContainerIterator it = graph.getNodeIterator(n);
-        while (it.hasNext()) {
-            NodeSerializer serializer = new NodeSerializer(it.next());
-            strings.add(serializer.getData());
-        }
-        writeFile(path, strings);
-    }
-
     public void exportNodes(String path) {
+        path = handlePath(path);
         for (NodeType n : NodeType.values()) {
             if (n != LABEL) {
                 String filepath = path + n.toString().toLowerCase() + ".txt";
-                exportNode(filepath, n);
+                List<String> strings = new ArrayList<>();
+                Container<Node>.ContainerIterator it = graph.getNodeIterator(n);
+                while (it.hasNext()) {
+                    NodeSerializer serializer = new NodeSerializer(it.next());
+                    strings.add(serializer.getData());
+                }
+                writeFile(filepath, strings, true);
             }
         }
     }
 
     public void exportEdges(String path) {
+        path = handlePath(path);
         Map<String, ArrayList<String>> strings = new HashMap<String, ArrayList<String>>();
 
         Iterator iter = graph.getRelationIterator();
         while (iter.hasNext()) {
             Relation r = (Relation) iter.next();
-            if (r.getName().matches(TXT_REGEX)) {
+            if (r.getName().matches(TXT_REGEX))
                 strings.put(r.getName(), new ArrayList<>());
-            } else {
+            else {
                 String s = r.getNodeTypeA().toString().toLowerCase() + "_" + r.getNodeTypeB().toString().toLowerCase() + "_" + r.getName();
                 strings.put(s, new ArrayList<>());
             }
@@ -71,14 +68,13 @@ public class PersistenceController {
                         for (int i = 0; i < node_list.size(); ++i) {
                             Node node2 = node_list.get(i);
                             EdgeSerializer serializer;
-                            if (rel.containsLabel()) {
+                            if (rel.containsLabel())
                                 serializer = new LabelSerializer(node1, node2);
-                            } else {
+                            else
                                 serializer = new EdgeSerializer(node1, node2);
-                            }
-                            if (rel.getName().matches(TXT_REGEX)) {
+                            if (rel.getName().matches(TXT_REGEX))
                                 strings.get(rel.getName()).add(serializer.getData());
-                            } else {
+                            else {
                                 String s = rel.getNodeTypeA().toString().toLowerCase() + "_" + rel.getNodeTypeB().toString().toLowerCase() + "_" + rel.getName();
                                 strings.get(s).add(serializer.getData());
                             }
@@ -93,15 +89,17 @@ public class PersistenceController {
         Iterator it = strings.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
-            writeFile(path + pair.getKey() + ".txt", (List<String>) pair.getValue());
+            writeFile(path + pair.getKey() + ".txt", (List<String>) pair.getValue(), true);
         }
 
     }
 
     public void importNodes(String path) {
+        path = handlePath(path);
         for (NodeType n : NodeType.values()) {
-            if (n != LABEL) {
-                String filepath = path + n.toString().toLowerCase() + ".txt";
+            String filepath = path + n.toString().toLowerCase() + ".txt";
+            File f = new File(filepath);
+            if (f.exists() && !f.isDirectory()) {
                 List<String> strings = readFile(filepath);
                 for (String s : strings) {
                     NodeSerializer serializer = new NodeSerializer(s);
@@ -117,51 +115,51 @@ public class PersistenceController {
     }
 
     public void importEdges(String path) {
+        path = handlePath(path);
         List<String> files = readDir(path);
         for (String f : files) {
-            String relation_name = null;
-            Relation r = null;
-            NodeType typeA = null;
-            NodeType typeB = null;
-            if (f.matches(DOUBLE_TXT_REGEX)) {
-                String[] parts = f.split("_");
-                typeA = NodeType.valueOf(parts[0].toUpperCase());
-                typeB = NodeType.valueOf(parts[1].toUpperCase());
-                relation_name = parts[2].substring(0, parts[2].length() - 4);
-            } else if (f.matches(TXT_REGEX)) {
-                String[] parts = f.split("_");
-                typeA = NodeType.valueOf(parts[0].toUpperCase());
-                typeB = NodeType.valueOf(parts[1].substring(0, parts[1].length() - 4).toUpperCase());
-                relation_name = f.substring(0, f.length() - 4).toLowerCase();
-            } else {
-                continue;
-            }
-            r = graph.getOrCreateRelation(typeA, typeB, relation_name);
-            List<String> strings = readFile(path + f);
-            for (String s : strings) {
-                EdgeSerializer serializer;
-                if (r.containsLabel()) {
-                    serializer = new LabelSerializer(graph, s, typeA, typeB);
+            if (f.endsWith(".txt")) {
+                String relation_name;
+                Relation r;
+                NodeType typeA;
+                NodeType typeB;
+                if (f.matches(DOUBLE_TXT_REGEX)) {
+                    String[] parts = f.split("_");
+                    typeA = NodeType.valueOf(parts[0].toUpperCase());
+                    typeB = NodeType.valueOf(parts[1].toUpperCase());
+                    relation_name = parts[2].substring(0, parts[2].length() - 4);
+                } else if (f.matches(TXT_REGEX)) {
+                    String[] parts = f.split("_");
+                    typeA = NodeType.valueOf(parts[0].toUpperCase());
+                    typeB = NodeType.valueOf(parts[1].substring(0, parts[1].length() - 4).toUpperCase());
+                    relation_name = f.substring(0, f.length() - 4).toLowerCase();
                 } else {
-                    serializer = new EdgeSerializer(graph, s, typeA, typeB);
+                    continue;
                 }
-                try {
-                    graph.addEdge(r.getId(), serializer.getNode1(), serializer.getNode2());
-                } catch (GraphException e) {
-                    e.printStackTrace();
+                r = graph.getOrCreateRelation(typeA, typeB, relation_name);
+                List<String> strings = readFile(path + f);
+                for (String s : strings) {
+                    EdgeSerializer serializer;
+                    if (r.containsLabel())
+                        serializer = new LabelSerializer(graph, s, typeA, typeB);
+                    else
+                        serializer = new EdgeSerializer(graph, s, typeA, typeB);
+                    try {
+                        graph.addEdge(r.getId(), serializer.getNode1(), serializer.getNode2());
+                    } catch (GraphException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
     }
 
     public void importGraph(String path) {
-        path = handlePath(path);
         importNodes(path);
         importEdges(path);
     }
 
     public void exportGraph(String path) {
-        path = handlePath(path);
         clearDir(path);
         exportNodes(path);
         exportEdges(path);
